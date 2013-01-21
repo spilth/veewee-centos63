8ee044fe62e4ca36b9eabe9be262842a54a76574
Brian Kelly <polymonic@gmail.com>
2013-01-21 08:57:54 -0500

    Build box without a GUI

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 gem install vagrant --no-ri --no-rdoc
 gem install veewee --no-ri --no-rdoc
 
-veewee vbox build 'centos63' --force --auto
+veewee vbox build 'centos63' --force --auto --nogui
 veewee vbox validate 'centos63'
 
 vagrant basebox export 'centos63'
